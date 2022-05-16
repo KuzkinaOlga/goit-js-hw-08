8bcab7b755053664f3267d050071b9bb673992ab
KuzkinaOlga <85559858+KuzkinaOlga@users.noreply.github.com>
2022-05-16 13:24:25 +0000

    Deploying to gh-pages from @ KuzkinaOlga/goit-js-hw-08@fb9c4aac311de6d63997a572d8b0439c838400c1 🚀

--- 03-feedback.ce1d0701.js
+++ 03-feedback.ce1d0701.js
@@ -4,4 +4,4 @@ var t=arguments[3],n="Expected a function",e=NaN,i="[object Symbol]",r=/^\s+|\s+
 },{}],"TTyb":[function(require,module,exports) {
 "use strict";var e=t(require("lodash.throttle"));function t(e){return e&&e.__esModule?e:{default:e}}const r="feedback-form-state",a=document.querySelector(".feedback-form");function o(e){let t=localStorage.getItem(r);(t=t?JSON.parse(t):{})[e.target.name]=e.target.value,localStorage.setItem(r,JSON.stringify(t))}function n(){let e=localStorage.getItem(r);e&&(e=JSON.parse(e),Object.entries(e).forEach(e=>{let[t,r]=e;a.elements[t].value=r}))}function l(e){e.preventDefault(),new FormData(a).forEach((e,t)=>console.log(e,t)),e.currentTarget.reset(),localStorage.removeItem(r)}a.addEventListener("input",(0,e.default)(o,500)),a.addEventListener("submit",l),n();
 },{"lodash.throttle":"FNyO"}]},{},["TTyb"], null)
-//# sourceMappingURL=/goit-js-hw-08/03-feedback.9ae11100.js.map
+//# sourceMappingURL=/goit-js-hw-08/03-feedback.ce1d0701.js.map
