cbeb42fbdb784f1515e5813b26c509a2e46b195e
KuzkinaOlga <o.kuzkina@gmail.com>
2022-05-16 15:23:06 +0200

    hw js 8

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,15 +1,19 @@
 
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
+
 const vimeoPlayerRef = document.querySelector('#vimeo-player');
 const player = new Player(vimeoPlayerRef);
 const LOCALSTORAGE_KEY = "videoplayer-current-time";
+
 const saveTime = function(event) {
     const time = event.seconds
     localStorage.setItem(LOCALSTORAGE_KEY, time)
-    }
+}
+    
 player.on('timeupdate', throttle(saveTime, 1000));
 player.on("play", setTime)
+
 function setTime() {
     const savedTime = localStorage.getItem(LOCALSTORAGE_KEY)
     if (savedTime) {
